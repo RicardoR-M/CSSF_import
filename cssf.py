3c90354dacfb33ccda2b714ed7dc4ec51b2e6707
Ricardo <1548550+RicardoR-M@users.noreply.github.com>
2024-08-01 00:25:16 -0500

    Update cssf.py
    Security update

--- cssf.py
+++ cssf.py
@@ -9,23 +9,12 @@ from campaña.repitencia_hfc import importa_repithfc
 from campaña.validaciones import importa_validaciones
 from campaña.seguimiento import importa_seguimiento
 
-engine = create_engine('mssql+pymssql://***REMOVED***:123@10.197.91.1:1433/CLARO_CSSF')
+engine = create_engine('mssql+pymssql://protected:protected@10.197.91.1:1433/CLARO_CSSF')
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-gcreds = {
-    "type": "service_account",
-    "project_id": "***REMOVED***",
-    "private_key_id": "***REMOVED***",
-    "private_key": "***REMOVED***",
-    "client_email": "cssf-653@***REMOVED***.iam.gserviceaccount.com",
-    "client_id": "***REMOVED***",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/cssf-653%40***REMOVED***.iam.gserviceaccount.com"
-}
+gcreds = {}
 
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(gcreds, scope)
 gc = gspread.authorize(credentials)
